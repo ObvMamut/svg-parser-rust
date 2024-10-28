e18e7c9cd504dc88393be75a46ae10116eaf2f54
ObvMamut <obvmamut@proton.me>
2024-10-28 19:24:43 +0100

    added horizontal and vertical line

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use plotly::Layout;
 use plotly::Plot;
 use plotly::Scatter;
-use rand_distr::num_traits::Pow;
 
 // Scatter Plots
-fn scatter_plot(points: Vec<Vec<f32>>) {
+fn scatter_plot(points: Vec<Vec<i32>>) {
     let mut x_values = vec![];
     let mut y_values = vec![];
 
@@ -27,8 +25,9 @@ fn scatter_plot(points: Vec<Vec<f32>>) {
 struct Path<'a> {
     path: &'a str,
     commands: Vec<String>,
-    points: Vec<Vec<f32>>,
-    stack: Vec<f32>,
+    points: Vec<Vec<i32>>,
+    stack: Vec<i32>,
+    synth_commands: Vec<Vec<String>>,
     n: i32,
 }
 
@@ -39,25 +38,9 @@ impl<'a> Path<'a> {
             commands: Self::dissect_path(path),
             points: vec![],
             stack: vec![],
-            n: 15,
+            synth_commands: vec![],
+            n: 10,
         }
-    }
-
-    fn calculate_total_length(&mut self) -> f32 {
-        // iterate through every command (Line or Bezier Curve)
-        // calculate it's length
-        // add it to the total
-        // return the total
-
-        let mut total: f32 = 0.0;
-
-        for x in 0..self.commands.len() {
-            match self.commands[x as usize].as_str() {
-                _ => {}
-            }
-        }
-
-        return total;
     }
 
     fn dissect_path(path: &str) -> Vec<String> {
@@ -92,57 +75,261 @@ impl<'a> Path<'a> {
         return result;
     }
 
-    fn synthesize_command(&mut self) -> Vec<String> {
+    fn synthesize(&mut self) {
+        // iterate through every term and transform every command into Cubic Bezier Curve
 
-        let mut stack = vec![];
+        let mut pointer = 0;
 
-        let mut last_command = vec![];
+        for command in self.commands.clone() {
+            match command.as_str() {
+                "M" => {
+                    // create the synth command
 
-        for e in self.commands.len() {
-            match self.commands[e].as_str() {
+                    let synth_cmd: Vec<String> = vec![
+                        self.commands.clone()[pointer].clone(),
+                        self.commands.clone()[pointer + 1].clone(),
+                        self.commands.clone()[pointer + 2].clone(),
+                    ];
+                    self.synth_commands.push(synth_cmd);
+
+                    // update stack
+
+                    self.stack = vec![
+                        self.commands.clone()[pointer + 1]
+                            .parse()
+                            .expect("not valid nr"),
+                        self.commands.clone()[pointer + 2]
+                            .parse()
+                            .expect("not valid nr"),
+                    ];
+                    pointer += 1;
+                }
+                "m" => {
+                    // create the synth command
+
+                    let synth_cmd: Vec<String> = vec![
+                        "M".to_string(),
+                        (self.commands.clone()[pointer + 1]
+                            .parse::<i32>()
+                            .expect("not valid nr")
+                            + self.stack[0])
+                            .to_string(),
+                        (self.commands.clone()[pointer + 2]
+                            .parse::<i32>()
+                            .expect("not valid nr")
+                            + self.stack[1])
+                            .to_string(),
+                    ];
+                    self.synth_commands.push(synth_cmd.clone());
+
+                    // update stack
+
+                    self.stack = vec![
+                        synth_cmd[1].parse().expect("not valid nr"),
+                        synth_cmd[2].parse().expect("not valid nr"),
+                    ];
+                    pointer += 1;
+                }
+                "L" => {
+                    let cb_curve = self.transform_line_to_cbezier(
+                        self.stack.clone(),
+                        vec![
+                            self.commands.clone()[pointer + 1]
+                                .parse::<i32>()
+                                .expect("not valid nr")
+                                .to_string(),
+                            self.commands.clone()[pointer + 2]
+                                .parse::<i32>()
+                                .expect("not valid nr")
+                                .to_string(),
+                        ],
+                    );
+
+                    self.synth_commands.push(cb_curve.clone());
+
+                    // update stack
+
+                    self.stack = vec![
+                        cb_curve[cb_curve.len() - 2].parse().expect("not valid nr"),
+                        cb_curve[cb_curve.len() - 1].parse().expect("not valid nr"),
+                    ];
+                    pointer += 1;
+                }
+                "l" => {
+                    let cb_curve = self.transform_line_to_cbezier(
+                        self.stack.clone(),
+                        vec![
+                            (self.commands.clone()[pointer + 1]
+                                .parse::<i32>()
+                                .expect("not valid nr")
+                                + self.stack[0])
+                                .to_string(),
+                            (self.commands.clone()[pointer + 2]
+                                .parse::<i32>()
+                                .expect("not valid nr")
+                                + self.stack[1])
+                                .to_string(),
+                        ],
+                    );
+
+                    self.synth_commands.push(cb_curve.clone());
+
+                    // update stack
+
+                    self.stack = vec![
+                        cb_curve[cb_curve.len() - 2].parse().expect("not valid nr"),
+                        cb_curve[cb_curve.len() - 1].parse().expect("not valid nr"),
+                    ];
+                    pointer += 1;
+                }
+                "H" => {
+                    // horizontal line
+
+                    let cb_curve = self.transform_line_to_cbezier(
+                        self.stack.clone(),
+                        vec![
+                            (self.commands.clone()[pointer + 1]
+                                .parse::<i32>()
+                                .expect("not valid nr"))
+                            .to_string(),
+                            (self.stack[1]).to_string(), // stack[1] = y, when horizontal line y = const
+                        ],
+                    );
+
+                    self.synth_commands.push(cb_curve.clone());
+
+                    // update stack
+
+                    self.stack = vec![
+                        cb_curve[cb_curve.len() - 2].parse().expect("not valid nr"),
+                        cb_curve[cb_curve.len() - 1].parse().expect("not valid nr"),
+                    ];
+                    pointer += 1;
+                }
+                "h" => {
+                    // horizontal line relative
+
+                    let cb_curve = self.transform_line_to_cbezier(
+                        self.stack.clone(),
+                        vec![
+                            (self.commands.clone()[pointer + 1]
+                                .parse::<i32>()
+                                .expect("not valid nr")
+                                + self.stack[0])
+                                .to_string(),
+                            (self.stack[1]).to_string(), // stack[1] = y, when horizontal line y = const
+                        ],
+                    );
+
+                    self.synth_commands.push(cb_curve.clone());
+
+                    // update stack
+
+                    self.stack = vec![
+                        cb_curve[cb_curve.len() - 2].parse().expect("not valid nr"),
+                        cb_curve[cb_curve.len() - 1].parse().expect("not valid nr"),
+                    ];
+                    pointer += 1;
+                }
+                "V" => {
+                    // vertical line
+
+                    let cb_curve = self.transform_line_to_cbezier(
+                        self.stack.clone(),
+                        vec![
+                            (self.stack[0]).to_string(), // stack[0] = x, when vertical line x = const
+                            (self.commands.clone()[pointer + 1]
+                                .parse::<i32>()
+                                .expect("not valid nr"))
+                            .to_string(),
+                        ],
+                    );
+
+                    self.synth_commands.push(cb_curve.clone());
+
+                    // update stack
+
+                    self.stack = vec![
+                        cb_curve[cb_curve.len() - 2].parse().expect("not valid nr"),
+                        cb_curve[cb_curve.len() - 1].parse().expect("not valid nr"),
+                    ];
+                    pointer += 1;
+                }
+                "v" => {
+                    // vertical line relative
+
+                    let cb_curve = self.transform_line_to_cbezier(
+                        self.stack.clone(),
+                        vec![
+                            (self.stack[0]).to_string(), // stack[0] = x, when vertical line x = const
+                            (self.commands.clone()[pointer + 1]
+                                .parse::<i32>()
+                                .expect("not valid nr")
+                                + self.stack[1])
+                                .to_string(),
+                        ],
+                    );
+
+                    self.synth_commands.push(cb_curve.clone());
+
+                    // update stack
+
+                    self.stack = vec![
+                        cb_curve[cb_curve.len() - 2].parse().expect("not valid nr"),
+                        cb_curve[cb_curve.len() - 1].parse().expect("not valid nr"),
+                    ];
+                    pointer += 1;
+                }
                 _ => {
+                    pointer += 1;
                 }
             }
         }
 
-
+        println!("{:?}", self.synth_commands)
     }
 
     fn get_points(&mut self) {
+        // synthesize transform every command except M to a bezier curve
 
-        // synthesize (remove every relative term)
-        // transform every command except M to a bezier curve
+        self.synthesize();
+
         // iterate through evert command :
         // - find the origin and target
         // - calculate the length
         // - compare the length to total length of whole SVG to see how many points to get (= nr_points)
         // - devide the height and lenth by the nr_points to find the x and y step
         // - array where you repeatedly add x_step and y_step to get list of points
-
-        let mut pointer = 0;
-
-        for element in self.commands.clone() {
-            match element.as_str() {
-
-                _ => {}
-            }
-            pointer += 1;
-        }
     }
 
+    fn transform_line_to_cbezier(&mut self, stack: Vec<i32>, end: Vec<String>) -> Vec<String> {
+        // transform line into cubic bezier C[start, 1control point, 2control point, end]
 
+        let mut cb_curve = vec!["C".to_string()];
+
+        // push stack as start and c1 = start
+
+        for _x in 0..2 {
+            cb_curve.push(stack[0].to_string());
+            cb_curve.push(stack[1].to_string());
+        }
+
+        // push c2, end = end
+
+        for _x in 0..2 {
+            cb_curve.push(end[0].clone());
+            cb_curve.push(end[1].clone());
+        }
+
+        return cb_curve;
+    }
+}
 
 fn main() {
-    let path = "M 175 200 l 150 0";
+    let path = "M 0 0 L 100 100 v 10";
     let mut pth = Path::init(path);
 
     pth.get_points();
 
-    // println!("{:?}", pth.points);
-
-    let total = pth.calculate_total_length();
-
-    println!("{:?}", total);
-
-    scatter_plot(pth.points);
+    //scatter_plot(pth.points);
 }
